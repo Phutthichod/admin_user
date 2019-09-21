048f0f49f947d3caee7be061d9d4849e61889702
Phutthichod <pin2017create@outlook.com>
2019-09-21 17:59:32 +0700

    two commit

--- view/user/data.php
+++ view/user/data.php
@@ -1,6 +1,6 @@
 <?php
 require '../../app/database.php';
  $data = new database();
- echo $data->select('select member.user_id,member.firstname , member.permission,member.status,member.lastname, member.email , count(*) AS "number" from member join user_plant on (member.user_id = user_plant.user_id)  group by  user_plant.user_id');
+ echo $data->select('select member.user_id,member.firstname , member.permission,member.status,member.lastname, member.email , count(user_plant.user_id) AS "number" from member LEFT JOIN user_plant on (member.user_id = user_plant.user_id)  group by  user_plant.user_id');
 
 ?>
